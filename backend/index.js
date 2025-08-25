e1d068f27a44e7d1d379569c16f4272a4f24b8a9
rishu <rrishu212@gmail.com>
2025-08-25 11:05:26 +0530

    LogIn & SignUp UI

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,15 @@ const port=process.env.PORT || 8000;
 
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.use(express.json());
 app.use(cookieParser());
